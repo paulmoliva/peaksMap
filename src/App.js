a91af6d4ce336bcf9133d063b210db0d023b5cd5
paulmoliva <paulmoliva@gmail.com>
2019-09-08 02:13:05 -0400

    layout new ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import Map from './components/Map'
+import { Layout, Dropdown, Menu, Breadcrumb, Icon } from 'antd';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 function App() {
+  const [drawerOpenState, toggleDrawer] = useState(false);
+  const districtMenu = (
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      defaultSelectedKeys={['2']}
+      style={{ lineHeight: '64px' }}
+    >
+      <Menu.Item key="1">Anchorage School District</Menu.Item>
+      <Menu.Item key="2">Alaska Statewide</Menu.Item>
+    </Menu>
+  )
+  const yearMenu = (
+    <Menu
+      theme="dark"
+      mode="horizontal"
+      defaultSelectedKeys={['2']}
+      style={{ lineHeight: '64px' }}
+    >
+      <Menu.Item key="1">2018</Menu.Item>
+      <Menu.Item key="2">2019</Menu.Item>
+    </Menu>
+  )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <div
+          style={{
+            minHeight: '75vh',
+            minWidth: '33vh',
+            background: 'gray',
+            position: "fixed",
+            top: '18vh',
+            left: drawerOpenState ? '0' : '-33vh',
+            zIndex: 3
+          }}
+        >Data Here</div>
+        <Header className="header">
+          <div className="logo" />
+          <Dropdown overlay={districtMenu} style={{margin: '5px'}}>
+            <a className="ant-dropdown-link" href="#">
+              Select Dataset <Icon type="down" />
+            </a>
+          </Dropdown>
+          ||||
+          <Dropdown overlay={yearMenu}>
+            <a className="ant-dropdown-link" href="#">
+              Select Year <Icon type="down" />
+            </a>
+          </Dropdown>
+        </Header>
+      <Layout>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item><a href='#'>Home</a></Breadcrumb.Item>
+            <Breadcrumb.Item><a href='#'>Education</a></Breadcrumb.Item>
+            <Breadcrumb.Item><a href='#'>Peaks Data</a></Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              background: '#fff',
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <Map slideOut={() => toggleDrawer(!drawerOpenState)} />
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
---
+++ src/components/Map.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Icon } from 'antd';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 61.19,
+      lng: -149.93
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    const iconStyle = {
+        pointer: 'cursor'
+    }
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '76vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAM_iXHF7lBSpOtMMcCkA7N8t70AkqPDmE' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <Icon
+            lat={61.1955413}
+            lng={-149.9337844}
+            type='home'
+            theme='twoTone'
+            twoToneColor='#eb2f96'
+            onClick={() => this.props.slideOut()}
+          />
+          <Icon
+            lat={61.1905413}
+            lng={-149.9337844}
+            type='home'
+            theme='twoTone'
+            twoToneColor='#52c41a'
+            onClick={() => this.props.slideOut()}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
